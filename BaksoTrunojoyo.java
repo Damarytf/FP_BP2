b1dfcc3e4b65107542bef43dda036ecb7a5b6a91
Damarytf <147298824+Damarytf@users.noreply.github.com>
2024-03-02 14:11:45 +0700

    Update BaksoTrunojoyo.java

--- BaksoTrunojoyo.java
+++ BaksoTrunojoyo.java
@@ -37,15 +37,21 @@ class Pembeli {
 class Makanan {
     private String namaMakanan;
     private int hargaMakanan;
+    private int stokMakanan;
 
-    Makanan(String n, int h) {
+    Makanan(String n, int h, int s) {
         namaMakanan = n;
         hargaMakanan = h;
+        stokMakanan = s;
         System.out.println("Menu " + namaMakanan + " diproses..");
     }
 
     public void setHargaMakanan(int h) {
         hargaMakanan = h;
+    }
+
+    public void setStokMakanan(int s) {
+        stokMakanan = s;
     }
 
     public String getNamaMakanan() {
@@ -54,6 +60,14 @@ class Makanan {
 
     public int getHargaMakanan() {
         return hargaMakanan;
+    }
+
+    public int getStokMakanan() {
+        return stokMakanan;
+    }
+
+    public void kurangiStokMakanan() {
+        stokMakanan--;
     }
 
     public String toString() {
@@ -65,10 +79,12 @@ class Makanan {
 class Minuman {
     private String namaMinuman;
     private int hargaMinuman;
+    private int stokMinuman;
 
-    Minuman(String n, int h) {
+    Minuman(String n, int h, int s) {
         namaMinuman = n;
         hargaMinuman = h;
+        stokMinuman = s;
         System.out.println("Menu " + namaMinuman + " diproses");
     }
 
@@ -84,6 +100,14 @@ class Minuman {
         return hargaMinuman;
     }
 
+    public int getStokMinuman() {
+        return stokMinuman;
+    }
+
+    public void kurangiStokMinuman() {
+        stokMinuman--;
+    }
+
     public String toString() {
         return namaMinuman + "[" + hargaMinuman + "]";
     }
@@ -92,13 +116,15 @@ class Minuman {
 public class BaksoTrunojoyo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Makanan[] daftarMakanan = new Makanan[8];
-        Minuman[] daftarMinuman = new Minuman[5];
+        final int MAX_MAKANAN = 8;
+        final int MAX_MINUMAN = 5;
+        Makanan[] daftarMakanan = new Makanan[MAX_MAKANAN];
+        Minuman[] daftarMinuman = new Minuman[MAX_MINUMAN];
         Pembeli[] daftarPembeli = new Pembeli[7];
         int totalTransaksi = 0;
 
-        Makanan makan = new Makanan("Pentol halus", 17000);
-        Minuman minum = new Minuman("Es Teh", 15000);
+        Makanan makan = new Makanan("Tahu", 17000, 10);
+        Minuman minum = new Minuman("EsJeruk", 15000, 15);
         Pembeli pembeli = new Pembeli("Budi", "5");
         int pilih = 0, pilih3;
 
@@ -130,31 +156,54 @@ public class BaksoTrunojoyo {
 
                             // Buat data Makanan
                             case 1:
-                                System.out.print("Nama Makanan = ");
-                                String nMakanan = sc.next();
-                                System.out.print("Harga = ");
-                                int hMakanan = sc.nextInt();
-                                makan = new Makanan(nMakanan, hMakanan);
+                                for (int i = 0; i < MAX_MAKANAN; i++) {
+                                    if (daftarMakanan[i] == null) {
+                                        System.out.print("Nama Makanan = ");
+                                        String nMakanan = sc.next();
+                                        System.out.print("Harga = ");
+                                        int hMakanan = sc.nextInt();
+                                        System.out.print("Stok = ");
+                                        int sMakanan = sc.nextInt();
+                                        daftarMakanan[i] = new Makanan(nMakanan, hMakanan, sMakanan);
+                                        break;
+                                    }
+                                }
                                 break;
 
                             // Lihat Menu
                             case 2:
-                                String tMakanan = makan.toString();
-                                System.out.println("Menu : " + tMakanan);
+                                System.out.println("Daftar Menu Makanan:");
+                                for (int i = 0; i < MAX_MAKANAN; i++) {
+                                    if (daftarMakanan[i] != null) {
+                                        System.out.println(daftarMakanan[i].getNamaMakanan() + ", Harga: " +
+                                                daftarMakanan[i].getHargaMakanan() +
+                                                ", Stok: " + daftarMakanan[i].getStokMakanan());
+                                    }
+                                }
                                 break;
 
                             // Ubah data
                             case 3:
-                                System.out.print("Harga Baru makanan = ");
-                                int hbMakanan = sc.nextInt();
-                                makan.setHargaMakanan(hbMakanan);
+                                System.out.println("Nama Makanan yang ingin diubah = ");
+                                String nbMakanan = sc.next();
+                                for (int i = 0; i < MAX_MAKANAN; i++) {
+                                    if (daftarMakanan[i] != null && daftarMakanan[i].getNamaMakanan().equals(nbMakanan)) {
+                                        System.out.print("Harga Baru makanan = ");
+                                        int hbMakanan = sc.nextInt();
+                                        System.out.println("Stok baru = ");
+                                        int nsMakanan = sc.nextInt();
+                                        daftarMakanan[i].setHargaMakanan(hbMakanan);
+                                        daftarMakanan[i].setStokMakanan(nsMakanan);
+                                        System.out.println("Data makanan berhasil diubah.");
+                                        break;
+                                    }
+                                }
                                 break;
 
                             // Kembali Menu Utama
                             case 4:
                                 System.out.println("Kembali ke Menu Utama..");
                                 break;
-
                         }
                     } while (pilih3 != 4);
                     break;
@@ -175,24 +224,45 @@ public class BaksoTrunojoyo {
 
                             // Buat data Minuman
                             case 1:
-                                System.out.print("Nama minuman = ");
-                                String nMinuman = sc.next();
-                                System.out.print("Harga = ");
-                                int hMinuman = sc.nextInt();
-                                minum = new Minuman(nMinuman, hMinuman);
+                                for (int i = 0; i < MAX_MINUMAN; i++) {
+                                    if (daftarMinuman[i] == null) {
+                                        System.out.print("Nama minuman = ");
+                                        String nMinuman = sc.next();
+                                        System.out.print("Harga = ");
+                                        int hMinuman = sc.nextInt();
+                                        System.out.print("Stok = ");
+                                        int sMinuman = sc.nextInt();
+                                        daftarMinuman[i] = new Minuman(nMinuman, hMinuman, sMinuman);
+                                        break;
+                                    }
+                                }
                                 break;
 
                             // Lihat Menu Minuman
                             case 2:
-                                String mMinuman = minum.toString();
-                                System.out.println("Minuman : " + mMinuman);
+                                System.out.println("Daftar Menu Minuman:");
+                                for (int i = 0; i < MAX_MINUMAN; i++) {
+                                    if (daftarMinuman[i] != null) {
+                                        System.out.println(daftarMinuman[i].getNamaMinuman() + ", Harga: " +
+                                                daftarMinuman[i].getHargaMinuman() +
+                                                ", Stok: " + daftarMinuman[i].getStokMinuman());
+                                    }
+                                }
                                 break;
 
                             // Ubah data
                             case 3:
-                                System.out.print("Harga baru = ");
-                                int hbMinuman = sc.nextInt();
-                                minum.setHargaMinuman(hbMinuman);
+                                System.out.print("Nama Minuman yang ingin diubah = ");
+                                String nbMinuman = sc.next();
+                                for (int i = 0; i < MAX_MINUMAN; i++) {
+                                    if (daftarMinuman[i] != null && daftarMinuman[i].getNamaMinuman().equals(nbMinuman)) {
+                                        System.out.print("Harga baru = ");
+                                        int hbMinuman = sc.nextInt();
+                                        daftarMinuman[i].setHargaMinuman(hbMinuman);
+                                        System.out.println("Data minuman berhasil diubah.");
+                                        break;
+                                    }
+                                }
                                 break;
 
                             // Kembali Ke Menu Utama
@@ -251,10 +321,26 @@ public class BaksoTrunojoyo {
                     System.out.println("Total transaksi: " + totalTransaksi);
             }
             if (pilih == 1) {
-                totalTransaksi += makan.getHargaMakanan();
+                for (int i = 0; i < MAX_MAKANAN; i++) {
+                    if (daftarMakanan[i] != null) {
+                        daftarMakanan[i].kurangiStokMakanan();
+                        totalTransaksi += daftarMakanan[i].getHargaMakanan();
+                        break;
+                    }
+                }
             } else if (pilih == 2) {
-                totalTransaksi += minum.getHargaMinuman();
+                for (int i = 0; i < MAX_MINUMAN; i++) {
+                    if (daftarMinuman[i] != null) {
+                        daftarMinuman[i].kurangiStokMinuman();
+                        totalTransaksi += daftarMinuman[i].getHargaMinuman();
+                        break;
+                    }
+                }
             }
         } while (pilih != 4);
     }
 }
+
+                                
+
+
