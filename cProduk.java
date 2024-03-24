b866d0fc00a23ab9390143e4a62760e119d6148b
Damarytf <147298824+Damarytf@users.noreply.github.com>
2024-03-24 22:29:34 +0700

    Update cProduk.java

--- cProduk.java
+++ cProduk.java
@@ -1,15 +1,14 @@
-package finalproject;
+package baksotrunojoyo;
 public class cProduk {
     //data
     private String nama, kode;
     private int harga, stok;
     //method
     cProduk(){
-        System.out.println("Constructor default");
     }
     cProduk(String n, int h, int s){
         nama=n; harga=h; stok=s;
-        System.out.println("Object "+nama+" dibuat..");
+        
     }
     public void setHarga(int h){ harga=h; }
     public void setStok(int s){ stok=s;}
@@ -18,17 +17,5 @@ public class cProduk {
     public int getStok(){ return stok; }
     public String toString(){
         return nama+" harga:"+harga+" stok:"+stok;
-    }
-}
-
-class cTransaksi{
-    private String namaProduk;
-    private int jumlahBeli;
-    private int totalHarga;
-    
-    public cTransaksi(String namaProduk, int jumlahBeli, int totalHarga){
-        this.namaProduk = namaProduk;
-        this.jumlahBeli = jumlahBeli;
-        this.totalHarga = totalHarga;
     }
 }
